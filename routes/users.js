1524768a25bb4d997b1ea02e7eab2c6902eee25e
Maksym Vaverchak <vaverchakm@gmail.com>
2022-03-13 20:38:51 -0400

    Workshop week 3

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,14 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+    // res.send('respond with a resource');
+    User.find()
+    .then(users => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users);
+    });
 });
 
 router.post('/signup', (req, res) => {
@@ -63,5 +69,6 @@ router.get('/logout', (req, res, next) => {
         return next(err);
     }
 });
+
 
 module.exports = router;
